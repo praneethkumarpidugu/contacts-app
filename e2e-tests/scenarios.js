6737f205c26762685175177a6f85c5771d6eef01
praneethkumarpidugu <pidugu2007@gmail.com>
2016-07-23 23:25:32 -0400

    clean up at few places for our contacts app

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -5,20 +5,20 @@
 describe('my app', function() {
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('should automatically redirect to /contacts when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/contacts");
   });
 
 
   describe('view1', function() {
 
     beforeEach(function() {
-      browser.get('index.html#!/view1');
+      browser.get('index.html#!/contacts');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
+    it('should render contacts when user navigates to /contacts', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 1/);
     });
